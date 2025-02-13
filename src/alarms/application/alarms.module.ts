51b8e5adb5dbc7a5eb678a61793b4a39acd07fc8
a-shpontak <andrii.shpontak@cowchain.io>
2025-02-13 06:26:01 +0200

    cqrs

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -2,10 +2,17 @@ import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
+import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { GetAlarmQueryHandler } from './queries/get-alarms.query-handler';
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory],
+  providers: [
+    AlarmsService,
+    AlarmFactory,
+    CreateAlarmCommandHandler,
+    GetAlarmQueryHandler,
+  ],
 })
 export class AlarmsModule {
   static withInfrastructure(infrastructureModule: Type | DynamicModule) {
